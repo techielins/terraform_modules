353c618a67999834741e625952ad6c6d4e66861b
Linson Joseph <68058598+techielins@users.noreply.github.com>
2023-05-16 16:17:42 +0530

    Create version.tf

---
+++ aws_aurora_postgres/version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_version = ">= 1.2.0"
+}
